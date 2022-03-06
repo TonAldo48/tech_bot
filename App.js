f4a0670357fbe962d1aec36f5be6a60a113654cb
David Nintang Junior <nintang48@gmail.com>
2022-03-06 16:49:58 +0000

    finished working on the send message component

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import {
+  SafeAreaView,
+  TextInput,
+  KeyboardAvoidingView,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import { FontAwesome } from 'react-native-vector-icons';
+import { Text, View } from 'react-native-ui-lib';
+import { SentMessage } from './src/components/sentmessage.component';
+import { ReceivedMessage } from './src/components/receivedmessage.component';
+import { Conversations } from './src/data/conversations.data';
 
 export default function App() {
+
+  var index = 1;
+  const renderItem = ({ item }) => {
+    index++;
+    if (item.isNew == false) {
+      return <SentMessage message={item.text} />
+    }
+    return <ReceivedMessage message={item.text} />
+  }
+
+  const [message, setMessage] = useState('');
+
+
+  useEffect(() => {
+    console.log(message);
+  }, [message])
+
+  const btnHandler = (message) => {
+    var kMessage = message;
+    console.log('button pressed')
+    console.log(kMessage)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ backgroundColor: 'black', flex: 1 }}>
+      <KeyboardAvoidingView behavior="padding" style={{ flex: 1, backgroundColor: 'black' }}
+      >
+        <View center spread row centerV style={{ height: 50, backgroundColor: 'rgb(20,20,20)' }}>
+          <Text white text40>TECHBOT</Text>
+        </View>
+        <View flex center style={{ backgroundColor: 'black' }}>
+          <FlatList
+            inverted
+            data={Conversations}
+            renderItem={renderItem}
+            keyExtractor={item => `${item.text} + ${1}`} />
+        </View>
+        <View center bg-black spread row centerV style={{ height: 50 }}>
+          <TextInput
+            keyboardAppearance='dark'
+            style={{
+              height: 40,
+              fontSize: 20,
+              flex: 1,
+              marginLeft: 10,
+              paddingLeft: 10,
+              borderRadius: 15,
+              backgroundColor: 'white',
+            }}
+            activeUnderlineColor='black'
+            selectionColor='blue'
+            defaultValue={message}
+            onChangeText={(text) => { setMessage(text) }}
+          />
+          <TouchableOpacity>
+            <FontAwesome
+              name={'send'}
+              size={30}
+              compact={true}
+              borderRadius={50}
+              style={{ color: 'white', borderRadius: 20, margin: 5 }}
+              onPress={() => { btnHandler(message); }}
+            />
+          </TouchableOpacity>
+        </View>
+
+      </KeyboardAvoidingView>
+    </SafeAreaView >
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/data/conversations.data.js
@@ -0,0 +1,19 @@
+export const Conversations = [
+  {
+    text: 'Made a purchase in the total of 7.00$',
+  },
+  {
+    text: 'Do you also carry these shoes in black?',
+    isNew: false
+  },
+  {
+    text: 'This might be the subject\nAnd the content is on a new line',
+    isNew: true
+  },
+  {
+    text: 'Yep',
+    isNew: true,
+  },
+];
+
+export default Conversations;
